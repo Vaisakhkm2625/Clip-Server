59ab2de21247ba643920ebaf0287e02e90a2e65e
Vaisakh K M <68694876+Vaisakhkm2625@users.noreply.github.com>
2021-12-10 19:50:48 +0530

    Create clipserve.py

---
+++ clipserve.py
@@ -0,0 +1,56 @@
+# Python 3 server example
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+import subprocess
+import re
+
+hostName = "0.0.0.0"
+serverPort = 8080
+
+regex = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+
+
+
+# file and directory listing
+def clipb():
+    returned_text = subprocess.check_output("termux-clipboard-get", shell=True, universal_newlines=True)
+    return returned_text
+
+def urlcheck(inpText):
+    if re.match(regex,inpText):
+        return "<a href=\""+ inpText+"\">"+inpText+"</a>"
+    else:
+        return inpText
+
+
+class MyServer(BaseHTTPRequestHandler):
+
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("clipboard", "utf-8"))
+        #self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>clipb "+ urlcheck(clipb()) + "</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
